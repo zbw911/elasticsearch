310f74484b81d57ab2c837b78ca3b76fb80f6de6
Artem Prigoda <artem.prigoda@elastic.co>
2023-09-26 00:29:53 +0200

    Pass shard's primary term to Engine#addSegmentGenerationListener
    So the SearchEngine can check that generation listeners are called on the
    same primaryTerm.

--- server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
+++ server/src/main/java/org/elasticsearch/index/shard/IndexShard.java
@@ -4173,6 +4173,6 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     }
 
     public void waitForSegmentGeneration(long segmentGeneration, ActionListener<Long> listener) {
-        getEngine().addSegmentGenerationListener(segmentGeneration, listener);
+        getEngine().addSegmentGenerationListener(getOperationPrimaryTerm(), segmentGeneration, listener);
     }
 }
